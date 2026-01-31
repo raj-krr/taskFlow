6d542d218915ad9acc0772204f840e5eea53b881
raj-krr <mr.rajkumar3579@gmail.com>
2026-01-31 13:51:48 +0530

    fix: update API base URL to production server

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://taskflow-0xfs.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
